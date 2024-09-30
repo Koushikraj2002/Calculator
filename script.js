f60762a9e80ee5d52552b2774461985a8b803011
Koushikraj2002 <koushik2002raj@gmail.com>
2024-09-30 14:20:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let display = document.getElementById('inputbox');
+// let buttons = document.querySelectorAll('button');
+let buttons = document.querySelectorAll('button');
+//buttons nodelist
+console.log(buttons);
+console.log(typeof(buttons));
+let string = ' ';
+let buttonsArray = Array.from(buttons);
+console.log(buttonsArray);
+buttonsArray.forEach(btn => {
+    
+    btn.addEventListener('click', (e) => {
+       
+        if (e.target.innerHTML == 'DEL') {
+            
+
+            string = string.substring(0, string.length - 1);
+            display.value = string
+        }
+        else if (e.target.innerHTML == 'AC') {
+
+            string = '';
+            display.value = string;
+        }
+        else if (e.target.innerHTML == '=') {
+
+            string = eval(string);
+            display.value = string;
+
+        }
+        else {
+            string += e.target.innerHTML;
+            display.value = string;
+
+        }
+    
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
